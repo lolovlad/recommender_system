bcfa3b88abec6bf67a51fc44c2cde572d6dbefb9
lolovlad <lolovlad33@gmail.com>
2026-02-10 21:27:13 +0400

    Laba 5

--- scripts/quality_gate.py
+++ scripts/quality_gate.py
@@ -5,13 +5,13 @@ def check_ndcg():
     client = mlflow.tracking.MlflowClient()
     runs = client.search_runs(
         experiment_ids=["0"],
-        order_by=["metrics.ndcg@10 DESC"],
+        order_by=["metrics.ndcg_10 DESC"],
         max_results=1
     )
 
-    ndcg = runs[0].data.metrics["ndcg@10"]
+    ndcg = runs[0].data.metrics["ndcg_10"]
     if ndcg <= 0.5:
-        raise ValueError(f"NDCG@10 too low: {ndcg}")
+        raise ValueError(f"NDCG_10 too low: {ndcg}")
 
     print("Quality gate passed:", ndcg)
 
